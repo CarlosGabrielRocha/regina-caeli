1d344487c5ccbbb19be66e38446627a1d53785d6
Carlos Gabriel <carlosgabrielweb2003@gmail.com>
2026-02-26 13:01:36 -0300

    fix: Change on pagination

--- src/components/paginationFilters/FilterTag.tsx
+++ src/components/paginationFilters/FilterTag.tsx
@@ -9,7 +9,7 @@ export function FilterTag({
   handleTagDelete,
 }: FilterTagProps) {
   return (
-    <div className="group relative flex flex-col justify-end max-w-30 md:max-w-70 mb-3 cursor-pointer shadow-md">
+    <div className="group relative flex flex-col justify-end max-w-30 lg:max-w-70 mb-3 cursor-pointer shadow-md">
       <div className="absolute right-0 -top-7 h-20 w-full items-start lg:opacity-30 lg:hover:opacity-100 transition-all">
         <div className="flex gap-2 backdrop-blur-xs p-1 rounded w-fit">
           <button
@@ -37,14 +37,12 @@ export function FilterTag({
         </div>
       </div>
       <button>
-        <Text
-          size="small"
-          type="span"
-          className="bg-highlight/30 border rounded-sm px-2 py-1 max-w-40 truncate block text-start"
+        <span
+          className="bg-highlight/30 border rounded-sm px-2 py-1 max-w-40 truncate block text-start text-xs lg:text-sm 2xl:text-md"
         >
           <span className="font-bold">{FilterTagType[type]}: </span>
           <span className="font-light">{children}</span>
-        </Text>
+        </span>
       </button>
     </div>
   );
