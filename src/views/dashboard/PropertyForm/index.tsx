6ab7526a3cef787555188ccb44d392e519f3ae57
Carlos Gabriel <carlosgabrielweb2003@gmail.com>
2026-02-26 12:48:51 -0300

    fix: Change on title length

--- src/views/dashboard/PropertyForm/index.tsx
+++ src/views/dashboard/PropertyForm/index.tsx
@@ -118,6 +118,7 @@ export default function PropertyForm({
               placeholder="Ex: Apartamento luxuoso no centro"
               register={register}
               error={errors.title}
+              maxLength={35}
               required
             />
           </div>
