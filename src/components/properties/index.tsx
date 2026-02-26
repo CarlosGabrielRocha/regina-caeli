6c071a6dab37310c26e1b5d2a37237e6a6312c56
Carlos Gabriel <carlosgabrielweb2003@gmail.com>
2026-02-26 12:39:28 -0300

    fix: Fix on properties grid

--- src/components/properties/index.tsx
+++ src/components/properties/index.tsx
@@ -11,7 +11,7 @@ interface PropertiesProps {
 
 export function PropertiesContainer({ children }: { children: ReactNode }) {
   return (
-    <div className="grid grid-cols-[repeat(auto-fill,minmax(13rem,max-content))] sm:grid-cols-[repeat(auto-fit,minmax(14rem,17rem))] lg:grid-cols-[repeat(4,minmax(14.5rem,17rem))] 2xl:grid-cols-[repeat(4,minmax(18rem,22rem))] gap-3 md:gap-6 w-full py-5 md:py-10 justify-center items-center">
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(13rem,max-content))] sm:grid-cols-[repeat(auto-fit,minmax(14rem,21rem))] lg:grid-cols-[repeat(4,minmax(14.5rem,17rem))] 2xl:grid-cols-[repeat(4,minmax(18rem,22rem))] gap-3 md:gap-6 w-full py-5 md:py-10 justify-center items-center">
       {children}
     </div>
   );
